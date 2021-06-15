9dabb854c043af081313b5abc02b45bec5c97a0b
wushufen <wusfun@foxmail.com>
2021-06-15 18:43:43 +0800

    css: running

--- src/components/running/index.jsx
+++ src/components/running/index.jsx
@@ -32,7 +32,15 @@ class Component extends React.Component{
                 className={classNames(styles.container)}
                 onClick={onStopAllClick}
             >
-                {'running'}
+                <div>
+                    <div className={classNames(styles.iconWrap)}>
+                        <span className={classNames(styles.icon, styles.icon1)} />
+                        <span className={classNames(styles.icon, styles.icon2)} />
+                        <span className={classNames(styles.icon, styles.icon3)} />
+                    </div>
+                    <p className={classNames(styles.text1)}>{'积木块运行中...'}</p>
+                    <span className={classNames(styles.text2)}>{'点击停止'}</span>
+                </div>
             </div>
         );
     }
