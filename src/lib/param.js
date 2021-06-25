bee7abb77ca93ada3fba063a90755d70bb763614
wushufen <wusfun@foxmail.com>
2021-06-25 21:54:16 +0800

    + param.js

---
+++ src/lib/param.js
@@ -0,0 +1,46 @@
+const getParam = function (name) {
+    if (RegExp(`[?&]${name}=([^?=&#]*)`).test(location)) {
+        let value = RegExp.$1;
+        value = decodeURIComponent(value);
+        try {
+            // Number.MAX_SAFE_INTEGER
+            if (value.length < 10) {
+                // eslint-disable-next-line no-eval
+                value = eval(value);
+            }
+            return value;
+        } catch (e) {
+            return value;
+        }
+    }
+};
+
+const setParam = function (name, value) {
+    let url = location.href;
+    url = url.replace(RegExp(`([?&])${name}=([^?=&#]*)`), `$1${name}=${value}`);
+    if (url === location.href) {
+        // todo
+    }
+    history.replaceState('', document.title, url);
+};
+
+/**
+ * get or set url param
+ * @param {string} name param name
+ * @param {*} value param value
+ * @returns {*} value
+ */
+const param = function (name, value) {
+    if (arguments.length === 2) {
+        return setParam(name, value);
+    }
+    return getParam(name);
+    
+};
+
+export {
+    param,
+    getParam,
+    setParam,
+    param as default,
+};
