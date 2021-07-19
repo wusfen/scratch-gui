844be888843747fa4bcfe021c82f2cc8db4f1c9e
wushufen <wusfun@foxmail.com>
2021-07-19 15:55:37 +0800

    fix: ID1002190, 1002189

--- src/components/submit-result-dialog/index.jsx
+++ src/components/submit-result-dialog/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import bindAll from 'lodash.bindall';
-import { OPERATE_TIME_2 ,timerType} from './../timer/data';
+import {OPERATE_TIME_2, timerType} from './../timer/data';
 
 
 import styles from './styles.css';
@@ -91,12 +91,13 @@ class Component extends React.Component{
         };
     }
     handleClose () {
-        if(/错误/.test(this.state.status)) {
+        if (/错误/.test(this.state.status)) {
             window.dispatchEvent(new Event(`noAction:${timerType.OPERATE}:${OPERATE_TIME_2}`));
         }
 
         if (this.state.isShowBackButton) {
-            window.bridge.emit('exitEditor');
+            // menu-bar.jsx
+            dispatchEvent(new Event('skipSaveExit'));
         }
 
         this.props.vm.stopAll();
