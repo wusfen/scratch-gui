76e0eb351c776bc77984b82c75d6ead4d419c40e
wushufen <wusfun@foxmail.com>
2021-07-23 06:07:15 +0800

    revert: _new.json => .json 恢复原始素材

--- src/containers/stage-selector.jsx
+++ src/containers/stage-selector.jsx
@@ -19,7 +19,7 @@ import {getEventXY} from '../lib/touch-utils';
 
 import StageSelectorComponent from '../components/stage-selector/stage-selector.jsx';
 
-import backdropLibraryContent from '../lib/libraries/backdrops_new.json';
+import backdropLibraryContent from '../lib/libraries/backdrops.json';
 import {handleFileUpload, costumeUpload} from '../lib/file-uploader.js';
 
 const dragTypes = [
