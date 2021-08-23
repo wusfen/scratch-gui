affe083b8234e379fd0844d824f04d71eaa48019
lianghonghua <honghua.liang@happ-seed.com>
2021-08-23 15:32:48 +0800

    修改角色原图大小上传，背景才拉大

--- src/containers/costume-tab.jsx
+++ src/containers/costume-tab.jsx
@@ -206,7 +206,7 @@ class CostumeTab extends React.Component {
                         this.props.onCloseImporting();
                     }
                 });
-            }, this.props.onCloseImporting);
+            }, this.props.onCloseImporting, true);
         }, this.props.onCloseImporting);
     }
     handleFileUploadClick () {
