07c9a31a359bcfa20600c917c9230752f40f80b2
wushufen <wusfun@foxmail.com>
2021-10-20 11:18:04 +0800

    ^ 更新素材库： 20211019的部分尺寸不合适

--- src/lib/libraries/wandou/backdrops.js
+++ src/lib/libraries/wandou/backdrops.js
@@ -1,5 +1,5 @@
 export default [
     ...require('./backdrops.20211012.json'),
-    ...require('./backdrops.20211019.json'),
+    ...require('./backdrops.20211020.json'),
     // ...require('./../backdrops.json'),
 ];
--- src/lib/libraries/wandou/costumes.js
+++ src/lib/libraries/wandou/costumes.js
@@ -1,5 +1,5 @@
 export default [
     ...require('./costumes.20211012.json'),
-    ...require('./costumes.20211019.json'),
+    ...require('./costumes.20211020.json'),
     // ...require('./../costumes.json'),
 ];
