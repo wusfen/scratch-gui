60e95065c21b527bd68ac08c6805e9557237f59c
chenyunqi <khat.yunqi@gmail.com>
2022-03-11 16:09:32 +0800

    feat: 解决分享没有给Android和iOS问题

--- src/playground/bridge.js
+++ src/playground/bridge.js
@@ -12,22 +12,27 @@ import getParam from '@/lib/param';
  */
 function emit (action, data) {
     console.info('[bridge.emit]', action, data);
+    const options = {
+        action,
+        data
+    };
     // 首次保存退出分享
     if (action === 'exitEditor') { // 调用了分享，就不调退出
         if (window._shareId) {
-            const userId = getParam('userId');
-            const shareId = window._shareId;
-            const base = getParam('base');
-            window.bridge.emit('share', {
+            options.action = action = 'share';
+            options.data = data = {
                 title: '分享作品',
                 content: '让爸爸妈妈扫描二维码\n试玩你的作品吧～',
                 mobileContent: '让爸爸妈妈试玩你的作品吧～',
                 shareTitle: '快来试玩我家宝贝在豌豆编程的创意作品吧，超赞~',
                 shareDetail: '点击查看宝贝的精彩作品',
                 url: `https://${base === 'uat' ? 'uat' : 'www'}.vipthink.cn/activity/market/crm-mobile-page/index.html#/workShare?channel=0&channelS=0&setRead=1&id=${shareId}&userId=${userId}`
-            });
+            };
+            const userId = getParam('userId');
+            const shareId = window._shareId;
+            const base = getParam('base');
+            window.bridge.emit('share', data);
             delete window._shareId;
-            return;
         }
     }
     const event = new Event('bridge.emit');
@@ -37,7 +42,7 @@ function emit (action, data) {
     };
     dispatchEvent(event);
 
-    var options = {action, data};
+    // var options = {action, data};
 
     // setTimeout处理：软键盘不收起，点击退出button，编辑器会闪退
     setTimeout(() => {
